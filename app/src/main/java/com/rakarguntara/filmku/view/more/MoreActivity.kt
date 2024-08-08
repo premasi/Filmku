b751ebdababf0bdc021a72de133837bd8f508423
Raka RG <rakaguntara10@gmail.com>
2024-08-08 12:35:55 +0700

    condition null for favorite

---
+++ app/src/main/java/com/rakarguntara/filmku/view/more/MoreActivity.kt
@@ -0,0 +1,21 @@
+package com.rakarguntara.filmku.view.more
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rakarguntara.filmku.R
+
+class MoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_more)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
